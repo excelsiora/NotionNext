3e8d7c01e2d226a15d01d258e80dc2ee392338b0
excelsiora <32334001+excelsiora@users.noreply.github.com>
2025-06-04 08:49:53 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Damon S.', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '莫听穿林打叶声，何妨吟啸且徐行', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://damon.s.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://damons-blog.notion.site/main?source=copy_link', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
